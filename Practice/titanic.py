61a6ff7e190bdfe0dcbe7918a6ee04af355d1545
rrcollab <richcollaborates@gmail.com>
2024-06-27 15:39:01 +0530

    New repositoy for Principles of Data Analytics

---
+++ Practice/titanic.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 08:17:35 2024
+
+@author: richc
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+data=pd.read_csv('./train.csv')
+len(data)
